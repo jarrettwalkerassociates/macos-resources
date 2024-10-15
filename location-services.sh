63a0b5cc852501bc00f06bb01d645937b8e2e940
jad1530 <159042749+jad1530@users.noreply.github.com>
2024-10-15 19:44:40 -0400

    update location-services.sh

--- location-services.sh
+++ location-services.sh
@@ -2,7 +2,7 @@
 # Enable location services and enforce ownership; path is no longer UUID dependent in Sierra
 /usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
 /usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd.notbackedup. LocationServicesEnabled -int 1
-/usr/sbin/chown -R _locationd:_locationd /var/db/locationd
+#/usr/sbin/chown -R _locationd:_locationd /var/db/locationd
 # Update Time Zone preference to auto-adjust based upon location
 /usr/bin/defaults write /Library/Preferences/com.apple.timezone.auto Active -bool YES
 /usr/bin/defaults write /Library/Preferences/com.apple.locationmenu ShowSystemServices -bool YES
