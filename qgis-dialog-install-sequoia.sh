e15e5643695dbde4f27cdbda6ab9923689c500df
jasonad123 <74881573+jasonad123@users.noreply.github.com>
2024-11-22 15:12:12 -0500

    update qgis-dialog-install-sequoia.sh

--- qgis-dialog-install-sequoia.sh
+++ qgis-dialog-install-sequoia.sh
@@ -1,60 +1,36 @@
 #!/bin/bash
 #script to prompt user to install QGIS via Dialog, if already installed, then use it as an opportunity to check version
 
-# #macOS version check for Gatekeeper bypass
-# #TODO get images from Sequoia
-# macOSVersion=$(sw_vers -productVersion)
-# sequoia="15.0.0"
-
-
-# if  [[ "${macOSVersion}" == "${sequoia}"* ]]; then
-#    echo "Mac is running Sequoia - use different Gatekeeper instructions"
-#    exit 0
-# else
-#     echo "Mac is not on an OK version."
-#     exit 1
-# fi
-
-#set dialog path
 dialogPath="/usr/local/bin/dialog"
-
-#dialog command file
 dialogCommandFile="/var/tmp/dialog.log"
 
-#dialog command function
 function dialog_command(){
-    /bin/echo "$@"  >> "$dialogCommandFile"
-    log_message "$@"
+    /bin/echo "$@" >> "$dialogCommandFile" 2>/dev/null
+    log_message "$@" 2>/dev/null
     sleep .1
 }
 
-#consistent Dialog variables
 dialogIcon="https://resources.jwainfrastructure.com/icns/qgis-styled.icns"
 dialogBannerImage="https://resources.jwainfrastructure.com/img/qgis-banner.png"
 
-#primary Dialog message - fresh install
 dialogTitleA="Installing QGIS"
-dialogMessageA="QGIS is an important part of our analysis pipeline. However, we’re unable to automatically install and launch it because of restrictions in macOS. However, you can manually install QGIS.  \n\nClick the “Install QGIS” button to open your browser and download the latest QGIS installer. After it downloads, run the installer.  \n\n### After the install  \n\n1. Go to System Settings > Privacy & Security\n2. Scroll down to find QGIS\n3. Click 'Open Anyway'\n4. Click 'Open' in the confirmation dialog"
-# dialogMessageA="QGIS is an important part of our analysis pipeline. However, we’re unable to automatically install and launch it because of restrictions in macOS. However, you can manually install QGIS.\n\nClick the “Install QGIS” button to open your browser and download the latest QGIS installer. After it downloads, run the installer.\n\nAfter QGIS is sucessfully installed, you’ll need to allow it to run. To do so, open Finder and click Applications, then find QGIS.\n\nHold down the Command (⌘) key and right-click (click with two fingers on a Mac trackpad), then click “Open”.<br><br>![Gatekeeper](https://resources.jwainfrastructure.com/img/qgis-launch-sonoma.png)"
+dialogMessageA="QGIS is an important part of our analysis pipeline. However, we're unable to automatically install and launch it because of restrictions in macOS. However, you can manually install QGIS.  \n\nClick the Install QGIS button to open your browser and download the latest QGIS installer. After it downloads, run the installer.  \n\n### After the install  \n\n1. Go to System Settings > Privacy & Security\n2. Scroll down to find QGIS\n3. Click 'Open Anyway'\n4. Click 'Open' in the confirmation dialog"
 dialogBannerTitleA="Installing QGIS"
 dialogButton1TextA="Install QGIS"
 
-#secondary Dialog message - QGIS already installed.
 dialogTitleB="QGIS already installed"
-dialogMessageB="QGIS is already installed. However, you can still use this app to download it again or update it.  \n\nFor consistency across the organization, we recommend using the **LTR version.**  \n\nIf you want to Download the “Latest” release, click on **Advanced installation**.  \n### After the install  \n\n1. Go to System Settings > Privacy & Security\n2. Scroll down to find QGIS\n3. Click 'Open Anyway'\n4. Click 'Open' in the confirmation dialog"
+dialogMessageB="QGIS is already installed. However, you can still use this app to download it again or update it.  \n\nFor consistency across the organization, we recommend using the **LTR version.**  \n\nIf you want to Download the Latest release, click on **Advanced installation**.  \n### After the install  \n\n1. Go to System Settings > Privacy & Security\n2. Scroll down to find QGIS\n3. Click 'Open Anyway'\n4. Click 'Open' in the confirmation dialog"
 dialogBannerTitleB="QGIS already installed"
 dialogButton1TextB="Download QGIS-LTR"
 
-
-app_name_1="QGIS-LTR.app"  # Replace with the actual application name you want to check
+app_name_1="QGIS-LTR.app"
 app_path_1="/Applications/$app_name_1"
 
-app_name_2="QGIS.app"  # Replace with the actual application name you want to check
-app_path_2="/Applications/$app_name_2"  # Typical location for applications
+app_name_2="QGIS.app"
+app_path_2="/Applications/$app_name_2"
 
-
-if [ -d "$app_path_1" ]; then
-    echo "QGIS_LTR is installed."
+if [ -d "$app_path_1" ] 2>/dev/null; then
+    echo "QGIS_LTR is installed." 2>/dev/null
 
     sleep 0.1
 
@@ -73,31 +49,29 @@ if [ -d "$app_path_1" ]; then
     --width 700 \
     --messagefont "size=18" \
     --height 505 \
-    --iconsize 120 \
+    --iconsize 120 2>/dev/null
 
-    #Very important that this part comes immediately after the dialog command
     dialogResults=$?
 
-    echo "Dialog exited with the following code: $dialogResults"
+    echo "Dialog exited with the following code: $dialogResults" 2>/dev/null
 
-    if [ "$dialogResults" = 0 ]; then
-        echo "Downloading latest QGIS LTR."
-        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg
-    elif [ "$dialogResults" = 2 ]; then
-        echo "User cancelled."
-    elif [ "$dialogResults" = 10 ]; then
-        echo "Exit key was used."
+    if [ "$dialogResults" = 0 ] 2>/dev/null; then
+        echo "Downloading latest QGIS LTR." 2>/dev/null
+        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg 2>/dev/null
+    elif [ "$dialogResults" = 2 ] 2>/dev/null; then
+        echo "User cancelled." 2>/dev/null
+    elif [ "$dialogResults" = 10 ] 2>/dev/null; then
+        echo "Exit key was used." 2>/dev/null
     else
-        echo "Dialog exited with an unexpected code."
-        echo "Could be an error in the dialog command"
-        echo "Could be the process killed somehow."
-        echo "Exit with an error code."
-        exit "$dialogResults"
+        echo "Dialog exited with an unexpected code." 2>/dev/null
+        echo "Could be an error in the dialog command" 2>/dev/null
+        echo "Could be the process killed somehow." 2>/dev/null
+        echo "Exit with an error code." 2>/dev/null
+        exit "$dialogResults" 2>/dev/null
     fi
 
-
-elif [ -d "$app_path_2" ]; then
-    echo "QGIS Latest is installed."
+elif [ -d "$app_path_2" ] 2>/dev/null; then
+    echo "QGIS Latest is installed." 2>/dev/null
 
     sleep 0.1
     
@@ -116,28 +90,28 @@ elif [ -d "$app_path_2" ]; then
     --width 700 \
     --messagefont "size=18" \
     --height 505 \
-    --iconsize 120 \
+    --iconsize 120 2>/dev/null
 
     dialogResults=$?
-    echo "Dialog exited with the following code: $dialogResults"
+    echo "Dialog exited with the following code: $dialogResults" 2>/dev/null
 
-    if [ "$dialogResults" = 0 ]; then
-        echo "Downloading latest QGIS LTR."
-        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg
-    elif [ "$dialogResults" = 2 ]; then
-        echo "User cancelled."
-    elif [ "$dialogResults" = 10 ]; then
-        echo "Exit key was used."
+    if [ "$dialogResults" = 0 ] 2>/dev/null; then
+        echo "Downloading latest QGIS LTR." 2>/dev/null
+        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg 2>/dev/null
+    elif [ "$dialogResults" = 2 ] 2>/dev/null; then
+        echo "User cancelled." 2>/dev/null
+    elif [ "$dialogResults" = 10 ] 2>/dev/null; then
+        echo "Exit key was used." 2>/dev/null
     else
-        echo "Dialog exited with an unexpected code."
-        echo "Could be an error in the dialog command"
-        echo "Could be the process killed somehow."
-        echo "Exit with an error code."
-        exit "$dialogResults"
+        echo "Dialog exited with an unexpected code." 2>/dev/null
+        echo "Could be an error in the dialog command" 2>/dev/null
+        echo "Could be the process killed somehow." 2>/dev/null
+        echo "Exit with an error code." 2>/dev/null
+        exit "$dialogResults" 2>/dev/null
     fi
 
 else
-    echo "QGIS is not installed."
+    echo "QGIS is not installed." 2>/dev/null
     
     "$dialogPath" \
     --title "$dialogTitleA" \
@@ -154,15 +128,15 @@ else
     --messagefont "size=18" \
     --width 700 \
     --height 500 \
-    --iconsize 120 \
+    --iconsize 120 2>/dev/null
 
     dialogResults=$?
 
-    echo "Dialog exited with the following code: $dialogResults"
+    echo "Dialog exited with the following code: $dialogResults" 2>/dev/null
 
-    if [ "$dialogResults" = 0 ]; then
-        echo "Downloading latest QGIS LTR."
-        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg
+    if [ "$dialogResults" = 0 ] 2>/dev/null; then
+        echo "Downloading latest QGIS LTR." 2>/dev/null
+        open https://qgis.org/downloads/macos/qgis-macos-ltr.dmg 2>/dev/null
         
         "$dialogPath" \
         --title "$dialogTitleA" \
@@ -178,20 +152,19 @@ else
         --messagefont "size=15" \
         --width 700 \
         --height 600 \
-        --iconsize 120 \
+        --iconsize 120 2>/dev/null
 
         dialogResults=$?
 
-    elif [ "$dialogResults" = 2 ]; then
-        echo "User cancelled."
-    elif [ "$dialogResults" = 10 ]; then
-        echo "Exit key was used."
+    elif [ "$dialogResults" = 2 ] 2>/dev/null; then
+        echo "User cancelled." 2>/dev/null
+    elif [ "$dialogResults" = 10 ] 2>/dev/null; then
+        echo "Exit key was used." 2>/dev/null
     else
-        echo "Dialog exited with an unexpected code."
-        echo "Could be an error in the dialog command"
-        echo "Could be the process killed somehow."
-        echo "Exit with an error code."
-        exit "$dialogResults"
+        echo "Dialog exited with an unexpected code." 2>/dev/null
+        echo "Could be an error in the dialog command" 2>/dev/null
+        echo "Could be the process killed somehow." 2>/dev/null
+        echo "Exit with an error code." 2>/dev/null
+        exit "$dialogResults" 2>/dev/null
     fi
-
 fi
